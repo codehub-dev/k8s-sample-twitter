3853b53fb3a0fa8aa5c5cd9b3ad9615587290989
su- <aaaiiiuuu35@gmail.com>
2020-05-09 21:45:29 +0900

    コード調整

--- user/dev_tools/initialize.ts
+++ user/dev_tools/initialize.ts
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
-const User = require('../models/user.js').User;
-const Follow = require('../models/follow.js').Follow;
+import * as mongoose from 'mongoose';
+const User = require('../src/models/user.js').User;
+const Follow = require('../src/models/follow.js').Follow;
 
 const dbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/user';
-const options = { useNewUrlParser: true, useCreateIndex: true };
+const options = { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true };
 if (process.env.MONGODB_ADMIN_NAME) {
+  // @ts-ignore
   options.user = process.env.MONGODB_ADMIN_NAME;
+  // @ts-ignore
   options.pass = process.env.MONGODB_ADMIN_PASS;
+  // @ts-ignore
   options.auth = { authSource: 'admin' };
 }
 
--- user/src/app.js
+++ user/src/app.js
@@ -7,7 +7,7 @@ app.use(morgan('short'));
 
 // database
 const dbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/user';
-const options = { useNewUrlParser: true };
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
 if (process.env.MONGODB_ADMIN_NAME) {
   options.user = process.env.MONGODB_ADMIN_NAME;
   options.pass = process.env.MONGODB_ADMIN_PASS;
